dd2426eaf1b8b69d10f18209a9f329e8109095d0
Robert O'Connor <rob@oconnor.ninja>
2018-04-05 17:32:22 -0400

    No longer need this

--- tools/github_repo_tool.rb
+++ tools/github_repo_tool.rb
@@ -6,7 +6,6 @@ require 'octokit'
 client = Octokit::Client.new(access_token: ENV['GITHUB_API_KEY'], per_page: 200)
 
 client.repositories('librehealthio').each do |repo|
-  next if repo.name === 'LibreEHR'
   client.update_repository "LibreHealthIO/#{repo.name}", has_issues: false, has_wiki: false, has_projects: false, has_downloads: true
   client.update_repository "LibreHealthIO/#{repo.name}", has_downloads: false if repo.name =~ /community-*/
 end
