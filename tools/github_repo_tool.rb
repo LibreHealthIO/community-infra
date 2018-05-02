6990ca7f6efe9652956e1e7a1b0d1c527dd3e731
Robert O'Connor <rob@oconnor.ninja>
2018-05-02 19:10:13 -0400

    add back EHR exception to repo tool

--- tools/github_repo_tool.rb
+++ tools/github_repo_tool.rb
@@ -6,6 +6,23 @@ require 'octokit'
 client = Octokit::Client.new(access_token: ENV['GITHUB_API_KEY'], per_page: 200)
 
 client.repositories('librehealthio').each do |repo|
-  client.update_repository "LibreHealthIO/#{repo.name}", has_issues: false, has_wiki: false, has_projects: false, has_downloads: true
-  client.update_repository "LibreHealthIO/#{repo.name}", has_downloads: false if repo.name =~ /community-*/
+  name = repo.name
+  if name =~ /lh-ehr-*/
+    client.update_repository "LibreHealthIO/#{repo.name}",
+                             has_issues: true,
+                             has_wiki: true,
+                             has_projects: true,
+                             has_downloads: true
+  end
+  unless name =~ /lh-ehr-*/
+    client.update_repository "LibreHealthIO/#{repo.name}",
+                             has_issues: false,
+                             has_wiki: false,
+                             has_projects: false,
+                             has_downloads: true
+  end
+  if name =~ /community-*/
+    client.update_repository "LibreHealthIO/#{repo.name}",
+                             has_downloads: false
+  end
 end
